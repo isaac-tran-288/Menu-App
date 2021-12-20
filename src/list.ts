4e8aaa9cbc58a87c8a291f09761e063238b28e21
Quang Huy Tran <traqy014@mymail.unisa.edu.au>
2021-12-20 22:18:39 +1030

    Change menu list JSON object structure

---
+++ src/list.ts
@@ -0,0 +1,55 @@
+export const DEFAULT_LIST = [
+    {
+        "text": "Login"
+    },
+    {
+        "text": "Product",
+        "children": [
+            {
+                "text": "Menswear",
+                "children": [
+                    {
+                        "text": "Shirts"
+                    },
+                    {
+                        "text": "Sneakers"
+                    },
+                    {
+                        "text": "Pants"
+                    }
+                ]
+            },
+            {
+                "text": "Womenswear",
+                "children": [
+                    {
+                        "text": "Shirts"
+                    },
+                    {
+                        "text": "Sneakers"
+                    },
+                    {
+                        "text": "Pants"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "text": "About"
+    },
+    {
+        "text": "Contact",
+        "children": [
+            {
+                "text": "Phone"
+            },
+            {
+                "text": "Email"
+            },
+            {
+                "text": "Office Location"
+            }
+        ]
+    }
+]
