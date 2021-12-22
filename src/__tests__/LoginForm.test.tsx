c2fa9cd88dad97441e1da4b76cb03fbd098cbe10
Quang Huy Tran <traqy014@mymail.unisa.edu.au>
2021-12-22 11:47:24 +1030

    Add tests for textfield input and clicking button of Login Form

--- src/__tests__/LoginForm.test.tsx
+++ src/__tests__/LoginForm.test.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
 import { shallow, mount } from 'enzyme';
 import LoginForm from '../LoginForm';
 import TextField from '@mui/material/TextField';
+import login from '../login';
 
+beforeAll(() => {
+    require("whatwg-fetch");
+});
 
 describe('Test Login Form', () => {
     const handleCloseMock = jest.fn();
+    const loginMock = jest.fn();
+
+    jest.doMock("../login", () => ({
+        __esModule: true,
+        default: loginMock
+    }));
+
     const wrapper = shallow(<LoginForm open={true} handleClose={handleCloseMock} />);
 
 
-    test('should render 2 <TextField /> element.', () => {
+    test('should render 2 <TextField /> elements', () => {
         expect(wrapper.find(TextField)).toHaveLength(2);
+        expect(wrapper.find(TextField).at(0).props().label).toEqual("Username");
+        expect(wrapper.find(TextField).at(1).props().label).toEqual("Password");
     });
 
 
-    // it('should show error when entered', () => {
-    //     wrapper.find('#name').simulate('change', { target: { value: '123' } });
-    //     expect(wrapper.find("#name").props().error).toBe(
-    //         true);
-    //     expect(wrapper.find("#name").props().helperText).toBe(
-    //         'Wrong Name format.');
-    // });
+    test('should call handle close function when clicking cancel', () => {
+        wrapper.find("#cancel").simulate('click');
+        expect(handleCloseMock).toHaveBeenCalled();
+    });
 
+    test('should call handle close function when clicking submit', () => {
+        // wrapper.find("#submit").simulate('click');
+        // expect(loginMock).toHaveBeenCalledWith("", "");
+        // expect(handleCloseMock).toHaveBeenCalled();
+
+    });
 
 });
